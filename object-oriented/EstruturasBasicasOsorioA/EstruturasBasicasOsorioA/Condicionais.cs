daa9ddd316f0e631dcea4f7e642b591fce074449
carolys <carolina_myasue@hotmail.com>
2020-08-28 21:28:55 -0300

    Create project EstruturasBasicas

---
+++ object-oriented/EstruturasBasicasOsorioA/EstruturasBasicasOsorioA/Condicionais.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturasBasicasOsorioA
+{
+    class Condicionais
+    {
+        /// <summary>
+        /// Estruta do IF
+        /// </summary>
+        public static void Se()
+        {
+            if (true)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
---
+++ object-oriented/EstruturasBasicasOsorioA/EstruturasBasicasOsorioA/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstruturasBasicasOsorioA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Imprimir mensagem no console
+            Console.WriteLine("Digite o seu nome:");
+            //Comando que faz leitura do teclado
+            string nome = Console.ReadLine();
+            Console.WriteLine("Nome digitado: " + nome);
+
+            Console.WriteLine("Digite a sua idade:");
+            //Converter tipos primitivos
+            //int idade = int.Parse(Console.ReadLine());
+            int idade = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Idade digitada: " + idade);
+
+            //Console.WriteLine(idade > 18 ? "Maior" : "Menor");
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Maior");
+            }
+            else
+            {
+                Console.WriteLine("Menor");
+            }
+
+            Console.WriteLine("\nAperte uma tecla para continuar...");
+            //Comando que espera uma tecla ser pressionada
+            Console.ReadKey();
+        }
+    }
+}
